ba676f18e6690db020e469b41425a9d338bc7cb2
Methawee Yingsung <6010110288@psu.ac.th>
2019-09-21 19:52:51 +0700

    Add random word

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 import './App.css';
+
+const word = ["Good", "Morning", "Teacher", "Goodbye", "See", "You", "Tomorrow"];
+const word_rand = word[Math.floor(Math.random()*word.length)];
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <WordCard value="hello"/>
+      <WordCard value= {word_rand} />
     </div>
     );
   }  
